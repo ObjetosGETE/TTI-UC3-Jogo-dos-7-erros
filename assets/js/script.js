ba5d690b841ceebb938d771231a383df5094b871
Renata Lima <64491568+renatadrake@users.noreply.github.com>
2024-12-11 18:24:29 -0300

    inclusao imagens

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,9 +10,54 @@ function controleBotoes() {
     $(".tela-cozinha").removeClass("d-none");
   });
 
+  $(".target1").on("click", function () {
+    $(this).find("img").removeClass("transparente");
+    $(".erro1").removeClass("transparente");
+    $(".reparar1").removeClass("d-none");
+  });
+
   $(".reparar1").on("click", function () {
     $(".tela-cozinha").addClass("d-none");
     $(".tela-cozinha-pronta").removeClass("d-none");
+    $(".continua1").removeClass("d-none");
+  });
+
+  $(".continua1").on("click", function () {
+    $(".tela-cozinha-pronta").addClass("d-none");
+    $(".tela-sala").removeClass("d-none");
+  });
+
+  $(".target2").on("click", function () {
+    $(this).find("img").removeClass("transparente");
+    $(".erro2").removeClass("transparente");
+    $(".reparar2").removeClass("d-none");
+  });
+
+  $(".reparar2").on("click", function () {
+    $(".tela-sala").addClass("d-none");
+    $(".tela-sala-pronta").removeClass("d-none");
+    $(".continua2").removeClass("d-none");
+  });
+
+  $(".continua2").on("click", function () {
+    $(".tela-sala-pronta").addClass("d-none");
+    $(".tela-quarto").removeClass("d-none");
+  });
+
+  $(".target3").on("click", function () {
+    $(this).find("img").removeClass("transparente");
+    $(".erro3").removeClass("transparente");
+    $(".reparar3").removeClass("d-none");
+  });
+
+  $(".reparar3").on("click", function () {
+    $(".tela-quarto").addClass("d-none");
+    $(".tela-quarto-pronta").removeClass("d-none");
+    $(".continua3").removeClass("d-none");
+  });
+
+  $(".continua3").on("click", function () {
+    $("#modalFinal").modal("show");
   });
 }
 
@@ -53,14 +98,6 @@ function resizeBodyConteudo() {
   }
 }
 
-$(document).ready(function () {
-  controleBotoes();
-  resizeBodyConteudo();
-  trocarImagem();
-
-  $(window).on("resize", resizeBodyConteudo);
-});
-
 function trocarImagem() {
   $(".reparar1").on("click", function () {
     setTimeout(function () {
@@ -72,7 +109,6 @@ function trocarImagem() {
           .attr("src", "assets/img/like.png")
           .removeClass("escalar");
       }, 300);
-      
     }, 2000);
 
     setTimeout(function () {
@@ -82,7 +118,82 @@ function trocarImagem() {
         $(".span-save1 img")
           .attr("src", "assets/img/salvar-alterado.png")
           .removeClass("escalar");
+        $("#audio-salvar")[0].play();
+      }, 300);
+    }, 3000);
+  });
+
+  $(".reparar2").on("click", function () {
+    setTimeout(function () {
+      $(".span-like2 img").addClass("escalar");
+
+      setTimeout(function () {
+        $("#audio-like")[0].play();
+        $(".span-like2 img")
+          .attr("src", "assets/img/like.png")
+          .removeClass("escalar");
+      }, 300);
+    }, 2000);
+
+    setTimeout(function () {
+      $(".span-save2 img").addClass("escalar");
+      $("#audio-like")[0].play();
+      setTimeout(function () {
+        $(".span-save2 img")
+          .attr("src", "assets/img/salvar-alterado.png")
+          .removeClass("escalar");
+        $("#audio-salvar")[0].play();
+      }, 300);
+    }, 3000);
+  });
+
+  $(".reparar3").on("click", function () {
+    setTimeout(function () {
+      $(".span-like3 img").addClass("escalar");
+
+      setTimeout(function () {
+        $("#audio-like")[0].play();
+        $(".span-like3 img")
+          .attr("src", "assets/img/like.png")
+          .removeClass("escalar");
+      }, 300);
+    }, 2000);
+
+    setTimeout(function () {
+      $(".span-save3 img").addClass("escalar");
+      $("#audio-like")[0].play();
+      setTimeout(function () {
+        $(".span-save3 img")
+          .attr("src", "assets/img/salvar-alterado.png")
+          .removeClass("escalar");
+        $("#audio-salvar")[0].play();
       }, 300);
     }, 3000);
   });
 }
+
+function somDeClique() {
+  $(".btn").on("click", function () {
+    var audio = document.getElementById("audio-clique");
+    if (audio) {
+      audio.play();
+    }
+  });
+}
+
+$(document).ready(function () {
+  $(".btn-iniciar").on("click", function () {
+    $("#modalIntroducao").modal("show");
+  });
+
+  $(".btn-comecar-jogo").on("click", function () {
+    $("#modalIntroducao").modal("hide");
+  });
+
+  controleBotoes();
+  resizeBodyConteudo();
+  trocarImagem();
+  somDeClique();
+
+  $(window).on("resize", resizeBodyConteudo);
+});
